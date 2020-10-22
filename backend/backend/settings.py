3903a02e873565816cfce771aa2c8df56f6d3f24
Mohan Zhou <mhzhou99@outlook.com>
2020-10-22 16:57:08 +0800

    Handle data upload
    - update .gitignore for sqlite3
    - add data upload handler: tested on postman.post.binary

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'China/Shanghai'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = False
 
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+APPEND_SLASH = True
