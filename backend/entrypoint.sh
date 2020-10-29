b30b6cdcce79ecb0e2b805ebea9646e41e71fd06
sjyang <jasysj@gmail.com>
2020-10-29 19:49:39 +0800

    fix empty page error

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,5 +2,5 @@
 
 chmod +x backend/static
 
-python manage.py runserver
-# gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 2
+# python manage.py runserver
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 2
