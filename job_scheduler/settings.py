cd63237aaed20b5edbb59a5899d07cdd99e9e5bd
kalineid <noob@mail.ustc.edu.cn>
2020-11-13 06:31:59 +0800

    Implement service

--- job_scheduler/settings.py
+++ job_scheduler/settings.py
@@ -1,1 +1,11 @@
-port = 12345
+import os
+
+PORT = 12345
+
+K8S_NAMESPACE = 'default'
+
+IMAGE_NAME = ''
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+DATA_DIR = os.path.join(BASE_DIR, 'worker/data')
