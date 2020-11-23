29262cf266a3194c1ac43a435fea9c6f0c396488
sjyang <gmail>
2020-11-23 10:32:26 +0800

    init sql server

--- restful_backend/entrypoint.sh
+++ restful_backend/entrypoint.sh
@@ -2,5 +2,8 @@
 
 chmod +x backend/static
 
+/wait.sh -t 120 mysql:3306
+
 # python manage.py runserver
+echo "from account.models import User; User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell
 gunicorn rishi.wsgi:application --bind 0.0.0.0:8000 --workers 2
