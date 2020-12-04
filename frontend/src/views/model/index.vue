6d776cf27bf83e834a698a0ec810e89205a9ca73
Chunyu Li <v-chunyuli@microsoft.com>
2020-12-04 15:32:49 +0800

    Apply config to all by default

--- frontend/src/views/model/index.vue
+++ frontend/src/views/model/index.vue
@@ -1,29 +1,49 @@
 <template>
   <div>
-    <h1 style="margin: 20px">Set Each Series</h1>
+    <h1 style="margin: 20px">Set Series</h1>
     <el-table
-      :data="series"
-      style="width: 100%"
+      :data="settingStr"
+      border
     >
-      <el-table-column type="expand">
+      <!-- <el-table-column type="expand">
         <template slot-scope="props">
           <series-set :id="props.row.ts_id" :features="features" @setDone="setDone(arguments)" />
         </template>
-      </el-table-column>
+      </el-table-column> -->
       <el-table-column
-        label="Series_Id"
-        prop="ts_id"
+        label="Model"
+        prop="model_name"
       />
       <el-table-column
-        :label="groupby_key_name"
-        prop="groupby_val"
+        label="Max Eval"
+        prop="max_eval"
       />
       <el-table-column
-        label="Setting Count"
-        prop="count"
+        label="Prediction Length"
+        prop="next_k_prediction"
       />
-    </el-table>
-    <el-button type="primary" style="display:block;margin:20px auto" @click="onSubmit">Submit</el-button>
+      <el-table-column
+        label="Auto tune"
+        prop="auto_tune"
+      />
+      <el-table-column
+        label="Eval Metrics"
+        prop="eval_metrics"
+      />
+      <el-table-column
+        label="Parameters"
+        prop="hyper_params"
+      />
+      <el-table-column
+        label="Operate"
+      >
+        <template slot-scope="props">
+          <el-button type="text" @click="DeleteSet(props.index)">delete</el-button>
+        </template></el-table-column></el-table>
+    <el-button v-if="seriesSettings.length >0" type="primary" style="display:block;margin:20px auto" @click="onSubmit">Submit</el-button>
+    <div style="display:block;margin:20px auto">
+      <series-set :features="features" @setDone="setDone(arguments)" />
+    </div>
   </div>
 </template>
 
@@ -43,20 +63,26 @@ export default {
       groupby_key_name: '',
       features: [],
       filters: [],
-      seriesSettings: []
+      seriesSettings: [],
+      settingStr: []
     }
   },
   created() {
     this.jobId = this.$route.query.job_id
-    this.fetchData()
+    // this.fetchData()
   },
   methods: {
+    DeleteSet(idx) {
+      this.seriesSettings.splice(idx, 1)
+      this.settingStr.splice(idx, 1)
+    },
     DisableSub() {
-      let disable = false
-      for (var i = 0; i < this.series.length; i++) {
-        disable = (this.series[i].count === 0)
-      }
-      return disable
+      return (this.seriesSettings.length === 0)
+      // let disable = false
+      // for (var i = 0; i < this.series.length; i++) {
+      //   disable = (this.series[i].count === 0)
+      // }
+      // return disable
     },
     minId() {
       let tmpmin = this.series[0]['ts_id']
@@ -90,28 +116,45 @@ export default {
         console.log(err)
       })
     },
+    Stringfy(obj) {
+      obj['auto_tune'] = String(obj['auto_tune'] !== 0)
+      let mtx_str = ''
+      obj['eval_metrics'].forEach(element => {
+        mtx_str += String(element)
+        mtx_str += ' '
+      })
+      obj['eval_metrics'] = mtx_str
+      let str = JSON.stringify(obj['hyper_params'])
+      str = str.substring(1, str.length - 1)
+      str = str.replace(',', '\n')
+      obj['hyper_params'] = str
+      return obj
+    },
     setDone(params) {
-      const id = params[0]
-      const applyAll = params[1]
-      const settings = params[2]
-      if (applyAll) {
-        const len = this.series.length
-        for (let i = 0; i < len; i++) {
-          const tmp = { ...settings }
-          tmp['ts_id'] = this.series[i].ts_id
-          const row = this.series[i]
-          row.count = row.count + 1
-          this.$set(this.series, i, row)
-          this.seriesSettings.push(tmp)
-        }
-      } else {
-        settings['ts_id'] = id
-        const idx = id - this.min_ts_id
-        const row = this.series[idx]
-        row.count = row.count + 1
-        this.$set(this.series, idx, row)
-        this.seriesSettings.push(settings)
-      }
+      // const id = params[0]
+      // const applyAll = params[1]
+      // const settings = params[2]
+      // if (applyAll) {
+      //   const len = this.series.length
+      //   for (let i = 0; i < len; i++) {
+      //     const tmp = { ...settings }
+      //     tmp['ts_id'] = this.series[i].ts_id
+      //     const row = this.series[i]
+      //     row.count = row.count + 1
+      //     this.$set(this.series, i, row)
+      //     this.seriesSettings.push(tmp)
+      //   }
+      // } else {
+      //   settings['ts_id'] = id
+      //   const idx = id - this.min_ts_id
+      //   const row = this.series[idx]
+      //   row.count = row.count + 1
+      //   this.$set(this.series, idx, row)
+      //   this.seriesSettings.push(settings)
+      // }
+      const settings = params[0]
+      this.seriesSettings.push(settings)
+      this.settingStr.push(this.Stringfy({ ...settings }))
     },
     onSubmit() {
       if (!this.DisableSub()) {
