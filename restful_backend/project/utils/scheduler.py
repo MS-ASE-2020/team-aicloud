a38278ee14a4ef74691f6d4a9615b58a9461db99
kalineid <noob@mail.ustc.edu.cn>
2020-12-17 23:17:02 +0800

    Make migrations & Fix packages

--- restful_backend/project/utils/scheduler.py
+++ restful_backend/project/utils/scheduler.py
@@ -1,7 +1,8 @@
 import rpyc
-import settings
+from django.conf import settings
 
 
 def commit_job(job_id):
     conn = rpyc.connect(settings.SCHEDULER_HOST, settings.SCHEDULER_PORT)
     conn.root.push(job_id)
+
